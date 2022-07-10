40358d5c58de9eec6f97d126ac191b4ba8dbc069
michaelanell <mickimaree@gmail.com>
2022-07-09 23:27:57 -0600

    file structure

---
+++ main.js
@@ -0,0 +1,29 @@
+const startForm = document.forms['user-info'];
+startForm.addEventListener('submit',redirect);
+
+function redirect(event) {
+    event.preventDefault();
+    alert(window.location.protocol + "//" + window.location.host + "/" );
+    //window.location.href = "";
+    alert('got here');
+    /*
+    var startOption = document.getElementsByName("start_option");
+    let result = "";
+    for (let i = 0; i < startOption.length; i++) {
+        if (startOption[i].checked) {
+            result = startOption[i].value;
+        }
+    }
+
+    if (result == "Create a Monthly Budget") {
+        window.location.href = "//budget.html";
+
+
+    } else if (result == "Create a Savings Goal") {
+        window.location.href = "http://www.w3schools.com";
+
+
+    } else {
+        window.location.href = "http://www.w3schools.com";
+    }*/
+    }
